c661c53f22e71a513826264e9e8d5fa51a827325
Art9Mor <99592441+Art9Mor@users.noreply.github.com>
2022-03-17 13:58:36 +0700

    Update ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -1,9 +1,9 @@
 public class ForLoop {
     public static void main(String[] args) {
         for (int i = 100; i >= 1; --i) {
-            if ((i % 4 == 0) && (i != 0)) ;
-            System.out.print("Number " + i + ",");
+            if (i % 4 == 0) {
+                System.out.print("Number " + i + ",");
+            }
         }
-
     }
 }
