e7152e1601f807262bd351b9224912d4bcf99830
Art9Mor <99592441+Art9Mor@users.noreply.github.com>
2022-03-21 01:03:20 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Person person1 = new Person("Ivan", "Petrovich", "Sidorov", 58);
         person1.printName();
-        person1.printAge();
+        person1.countAge();
 
         Person person2 = new Person("Vitalina", "Pavlovna", "Strogova", 24);
         person2.printName();
-        person2.printAge();
+        person2.countAge();
 
     }
 }
