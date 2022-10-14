0eafd1e5bb8c60b5e9cabf57747e4abfc39a8f00
Art9Mor <99592441+Art9Mor@users.noreply.github.com>
2022-10-14 09:57:58 +0700

    Update DigitsNumbers.java
    Rebuilding

--- DigitsNumbers.java
+++ DigitsNumbers.java
@@ -1,15 +1,38 @@
 import java.util.Scanner;
-public class DigitsNumbers {
-    public static void main(String [] args){
-        Scanner scanner = new Scanner(System.in);
-        int count = scanner.nextInt();
-        int result = 100;
-        for (int i = 0; i < count; i++){
-            int number = scanner.nextShort();
-            if((number % 4 == 0) && (number != 0)){
-                result++;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input a number: ");
+        int number = in.nextInt();
+
+        String numberToStr = String.valueOf(number);
+        String[] strToArray = numberToStr.split("");
+        int[] numbers = new int[strToArray.length];
+
+        for (int i = 0; i < strToArray.length; i++) {
+            numbers[i] = Integer.valueOf(strToArray[i]);
+            System.out.println(numbers[i]);
+        }
+        int sum = 0;
+        for (int i: numbers) {
+            sum = sum + i;
+        }
+        int sumqwe = 0;
+        for (int i: numbers) {
+            if (i%2 != 0) {
+                sumqwe += i;
             }
         }
-        System.out.println(result);
+        int max = 0;
+        for (int i: numbers) {
+           max = Math.max(max, i);
+
+        }
+
+        System.out.println("Сумма всех цифр = " + sum);
+        System.out.println("Сумма нечётных цифр = " + sumqwe);
+        System.out.println("Максимальная цифра = " + max);
     }
 }
